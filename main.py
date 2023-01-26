afbadbaedf8a979489c5bc72e996306a3311ddf3
spiky <87581740+sspiky@users.noreply.github.com>
2023-01-26 13:31:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ with open('cookie.txt', 'r') as f:
 
 token = requests.post(url='https://auth.roblox.com/v2/logout', headers={'cookie': roblosecurity}).headers["x-csrf-token"]
 user_id = requests.get(url='https://users.roblox.com/v1/users/authenticated', headers={'cookie': roblosecurity}).json()["id"]
-
-getuaids = requests.get(url=f"https://inventory.roblox.com/v1/users/{user_id}/items/0/{item_id}")
-uaids = [i['instanceId'] for i in getuaids.json()['data']]
+uaids = [i['instanceId'] for i in requests.get(url=f"https://inventory.roblox.com/v1/users/{user_id}/items/0/{item_id}").json()['data']]
 
 for i in uaids:
     res = requests.patch(url=f'https://economy.roblox.com/v1/assets/{item_id}/resellable-copies/{i}', json={"price":price}, headers={'accept': 'application/json', 'content-type':'application/json', 'cookie': roblosecurity, 'x-csrf-token': token})
