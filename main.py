5715b6bf7eb1c2e0c31627e363e302af71ac7e78
spiky <87581740+sspiky@users.noreply.github.com>
2023-01-27 06:35:09 -0800

    hiiiii

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ user_id = requests.get(url='https://users.roblox.com/v1/users/authenticated', he
 uaids = [i['instanceId'] for i in requests.get(url=f"https://inventory.roblox.com/v1/users/{user_id}/items/0/{item_id}").json()['data']]
 
 for i in uaids:
-    res = requests.patch(url=f'https://economy.roblox.com/v1/assets/{item_id}/resellable-copies/{i}', json={"price":price}, headers={'accept': 'application/json', 'content-type':'application/json', 'cookie': roblosecurity, 'x-csrf-token': token})
+    res = requests.patch(url=f'https://economy.roblox.com/v1/assets/{item_id}/resellable-copies/{i}', json={"price":price}, headers={'cookie': roblosecurity, 'x-csrf-token': token})
     print(f"successfully listed UAID {i} for {price} robux :)" if res.status_code == 200 else print("fail"))
 
 input(" ")
